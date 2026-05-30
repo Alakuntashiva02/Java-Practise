a9e0a2ff9ee97952a5426825b18336c348222311
Shiva kumar <shivakumarofficial123@gmail.com>
2026-05-30 16:09:11 +0530

    Fixed the error in MultiOf7

--- src/com/basic/MultiOf7.java
+++ src/com/basic/MultiOf7.java
@@ -3,7 +3,7 @@ package com.basic;
 public class MultiOf7 {
 public static void main(String args[]) {
 	
-	for (int i=0; i>=70; i++) {
+	for (int i=0; i<=70; i++) {
 		if(i%7==0) {
 			System.out.println(i);
 		}
